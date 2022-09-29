fea21faa5a1fc6a3892f35ee6800de63b27c134a
GabrielCastro <castroo.gabriel99@gmail.com>
2022-09-29 18:16:26 -0300

    Desafio Inicial 01 da trilha de´Pyhton

---
+++ AprendendoPython/DesafioInicial01.py
@@ -0,0 +1,25 @@
+# DICAS SOBRE PYTHON:
+# FUNÇÃO input(): Ela recebe como parâmetro uma String que será visível ao usuário,
+# onde geralmente informa que tipo de informação ele está esperando receber;
+# FUNÇÃO print(): Ela é a responsável por imprimir os dados e informar os valores no terminal;
+# MÉTODO split(): permite dividir o conteúdo da variável de acordo com as condições especificadas
+# em cada parâmetro da função ou com os valores predefinidos por padrão;
+# TODO: Calcule o ICM da comunicação dos Palatír de Sauron e Saruman, com 2 casas decimais no espaço #em branco abaixo:
+
+# Soluções:
+# Solução01: Resolução feita de acordo com o modo de entrada sugerido no desafio.
+entrada = input().split()
+
+distancia = int(entrada[0])
+diametro1 = int(entrada[1])
+diametro2 = int(entrada[2])
+ICM = float(distancia/(diametro1 + diametro2))
+print(f'{ICM:.2f}')
+
+# Solução02: Uma solução que dialoga com o usuário final.
+distanciaSS = int(input("A distância entre Sauron e Saruman é: "))
+diametroS1 = int(input("O diâmetro do Palantír de Sauron é: "))
+diametroS2 = int(input("O diâmetro do Palantír de Saruman é: "))
+
+ICM = float(distanciaSS/(diametroS1 + diametroS2))
+print(f'O ICM da comunicação dos Palatír de Sauron e Saruman é {ICM:.2f}!')
