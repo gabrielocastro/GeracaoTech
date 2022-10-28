73f6f8bb8bc6634bed0eb507ead217bd11914f76
GabrielCastro <castroo.gabriel99@gmail.com>
2022-10-28 13:00:49 -0300

    Desafio Intermediário 01 do Bootcamp de Python

--- AprendendoPython/Desafios de Código/Desafio Intermediário/DesafioIntermediário01.py
+++ AprendendoPython/Desafios de Código/Desafio Intermediário/DesafioIntermediário01.py
@@ -3,7 +3,7 @@
 # onde geralmente informa que tipo de informação ele está esperando receber;
 # FUNÇÃO print(): Ela é a responsável por imprimir os dados e informar os valores no terminal;
 # MÉTODO ord(): Retorna o valor  ASCII de cada letra ou símbolo do teclado;
-# tODO: De acordo com a entrada, imprima a posição dessa letra no Alfabeto;
+# TODO: De acordo com a entrada, imprima a posição dessa letra no Alfabeto;
 import string
 
 letra = input().upper()
