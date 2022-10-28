37b36014485762f0310b8f7021968199e48edc83
GabrielCastro <castroo.gabriel99@gmail.com>
2022-10-28 12:16:27 -0300

    Desafio Inicial 03 da trilha de Python

--- AprendendoPython/DesafioInicial03.py
+++ AprendendoPython/DesafioInicial03.py
@@ -2,7 +2,7 @@
 # FUNÇÃO input(): Ela recebe como parâmetro uma String que será visível ao usuário,
 # onde geralmente informa que tipo de informação ele está esperando receber;
 # FUNÇÃO print(): Ela é a responsável por imprimir os dados e informar os valores no terminal;
-# MÉTODO split(): permite dividir o conteúdo da variável de acordo com as condições especificadas
+# mÉTODO split(): permite dividir o conteúdo da variável de acordo com as condições especificadas
 # em cada parâmetro da função ou com os valores predefinidos por padrão;
 
 # Abaixo segue um exemplo de código que você pode ou não utilizar
