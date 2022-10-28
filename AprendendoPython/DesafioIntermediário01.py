29afc8408c890500bd1cb9d31f6ca8c767589365
GabrielCastro <castroo.gabriel99@gmail.com>
2022-10-28 12:43:49 -0300

    Desafio Intermediário 01 da trilha de Python

---
+++ AprendendoPython/DesafioIntermediário01.py
@@ -0,0 +1,12 @@
+# DICAS SOBRE PYTHON:
+# FUNÇÃO input(): Ela recebe como parâmetro uma String que será visível ao usuário,
+# onde geralmente informa que tipo de informação ele está esperando receber;
+# FUNÇÃO print(): Ela é a responsável por imprimir os dados e informar os valores no terminal;
+# MÉTODO ord(): Retorna o valor  ASCII de cada letra ou símbolo do teclado;
+# tODO: De acordo com a entrada, imprima a posição dessa letra no Alfabeto;
+import string
+
+letra = input().upper()
+alfabeto = list(string.ascii_uppercase)
+posicao_letra = (alfabeto.index(letra)+1)
+print(posicao_letra)
